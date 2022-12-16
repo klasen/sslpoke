87ae7611f68dfb51b17b52ba80e4331ab934d2c1
Norbert Klasen <norbert.klasen@microfocus.com>
2022-12-16 11:39:01 +0100

    Updat to JUnit 5 and add more tests.

--- src/test/java/SSLPokeTest.java
+++ src/test/java/SSLPokeTest.java
@@ -1,9 +1,13 @@
-import org.junit.Test;
-
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
+
+import org.junit.jupiter.api.Test;
+import org.junitpioneer.jupiter.SetSystemProperty;
 
 /**
  * Unit test.
@@ -21,13 +25,59 @@ public class SSLPokeTest {
         assertNotNull(result);
     }
 
-    @Test(expected=javax.net.ssl.SSLHandshakeException.class)
-    public void selfSignedBadSslCom() throws UnknownHostException, IOException {
-        SSLPoke.connect("self-signed.badssl.com", 443);
+    @Test
+    public void unknownHost() throws UnknownHostException, IOException {
+
+        Exception exception = assertThrows(java.net.UnknownHostException.class,
+                () -> SSLPoke.connect("host.invalid", 443));
     }
 
-    @Test(expected=java.net.UnknownHostException.class)
-    public void hostUnknown() throws UnknownHostException, IOException {
-        SSLPoke.connect("host.unknown", 443);
+    @Test
+    public void expired() throws UnknownHostException, IOException {
+        Exception exception = assertThrows(javax.net.ssl.SSLHandshakeException.class,
+                () -> SSLPoke.connect("expired.badssl.com", 443));
+
+    }
+
+    @Test
+    public void wrongHost() throws UnknownHostException, IOException {
+        Exception exception = assertThrows(javax.net.ssl.SSLHandshakeException.class,
+                () -> SSLPoke.connect("wrong.host.badssl.com", 443));
+    }
+
+    @Test
+    public void selfSigned() throws UnknownHostException, IOException {
+        Exception exception = assertThrows(javax.net.ssl.SSLHandshakeException.class,
+                () -> SSLPoke.connect("self-signed.badssl.com", 443));
+    }
+
+    @Test
+    public void untrustedRoot() throws UnknownHostException, IOException {
+        Exception exception = assertThrows(javax.net.ssl.SSLHandshakeException.class,
+                () -> SSLPoke.connect("untrusted-root.badssl.com", 443));
+    }
+
+    @Test
+    @SetSystemProperty(key = "jdk.tls.client.protocols", value = "SSLv3")
+    public void sslV3() throws UnknownHostException, IOException {
+        Exception exception = assertThrows(javax.net.ssl.SSLHandshakeException.class,
+                () -> SSLPoke.connect("www.badssl.com", 443));
+    }
+
+    @Test
+    @SetSystemProperty(key = "com.sun.net.ssl.checkRevocation", value = "false")
+    @SetSystemProperty(key = "com.sun.security.enableCRLDP", value = "true")
+    public void ignoreRevocation() throws UnknownHostException, IOException {
+        assertNotNull(SSLPoke.connect("revoked.badssl.com", 443));
+    }
+
+    @Test
+    @SetSystemProperty(key = "com.sun.net.ssl.checkRevocation", value = "true")
+    @SetSystemProperty(key = "com.sun.security.enableCRLDP", value = "true")
+    public void revoked() throws UnknownHostException, IOException {
+        assertEquals("true", System.getProperty("com.sun.net.ssl.checkRevocation"));
+        Exception exception = assertThrows(javax.net.ssl.SSLHandshakeException.class,
+                () -> SSLPoke.connect("revoked.badssl.com", 443));
+        assertTrue(exception.getMessage().contains("Certificate has been revoked"));
     }
 }
